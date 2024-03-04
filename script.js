bc1088da4649f0b0b3d7c33c86d2f3157fbe9891
JUNAIDUL ISLAM JARIF <153998279+jarif-junaidul@users.noreply.github.com>
2024-03-04 18:03:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.JoinButton').onclick = function changePage() {
+    window.location.href = 'creatAccount.html';
+}
+document.querySelector('.LogInButton').onclick = function changePage() {
+    window.location.href = 'profile.html';
+}
+document.querySelector('.FPText').onclick = function changePage() {
+    window.location.href = 'FP.html';
+}
